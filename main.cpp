ea2283c6e3d50721dd8c1af73f42cefdfa79efa0
arnoldruiz115 <arnoldruiz115@csu.fullerton.edu>
2018-11-09 19:21:11 -0800

    Create main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string w1,w2;
+	cout << "Edit Distance Calculator" << endl;
+	cout << "Enter first word: ";
+	cin >> w1;
+	cout << "Enter second word: ";
+	cin >> w2;
+}
