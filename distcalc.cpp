854f96cb4113c96fdc55f8790507088f6947fc5c
arnoldruiz115 <arnoldruiz115@csu.fullerton.edu>
2018-11-14 00:45:03 -0800

    Alignment now shows properly

--- distcalc.cpp
+++ distcalc.cpp
@@ -55,31 +55,49 @@ void eDistance(string w1, string w2){
 	cout << endl;
 	
 	cout << "\nEdit distance is: " << eMatrix[rows-1][cols-1] << endl;
+	
 	//Finding Alignment
 	int i = rows-1;
 	int j = cols-1;
 	int tempMin, diag, top, left;
 	bool diagMin = false;
-	while(i > 0 && j > 0){
-		diag = eMatrix[i-1][j-1];
-		top =  eMatrix[i-1][j];
-		left = eMatrix[i][j-1];
+	string align1 = "";
+	string align2 = "";
+	while((i != 0) || (j != 0)){
+		//in case traversal is along the edge, dont check out of bounds		
+		if(j == 0){diag = 999; left = 999;}
+		else if(i == 0){diag = 999; top = 999;}
+		else{
+			diag = eMatrix[i-1][j-1];
+			top =  eMatrix[i-1][j];
+			left = eMatrix[i][j-1];
+		}
 		tempMin = min({diag, top, left});
 		if(tempMin == diag){
 			 diagMin = true;
-			 cout << "Diag Min" << endl;
+			 //Diagonal is Minimum
+			 align1 += word1[i];
+			 align2 += word2[j];
 			 i--;
 			 j--;
 		 }
 		 if(!diagMin){
-			 if(tempMin == top) {cout << "top min" << endl;
-			 i--;
+			 if(tempMin == top) {
+				 //top is minimum
+				 align1 += word1[i];
+				 align2 += "_";
+				 i--;
 		 }
 		 else{
-			 cout << "left min" << endl;
+			 //left is minimum
+			 align2 += word2[j];
+			 align1 += '_';
 			 j--;
 			}
 		 }
-		 diagMin = false;	
+		 diagMin = false;
 	}
+	reverse(align1.begin(), align1.end());
+	reverse(align2.begin(), align2.end());
+	cout << "\nAlignment: \n" << align1 << endl << align2;
 }
